a93f9b2fbaa6bf1dfb7215ac62e0bb51752d077a
Pipsik <531621@mail.ru>
2016-09-28 15:05:46 +0300

    added text fields

--- app/assets/javascripts/sites/create/create.component.js
+++ app/assets/javascripts/sites/create/create.component.js
@@ -19,46 +19,12 @@ component('createView', {
       $scope.models = {
           selected: null,
           templates: [
-              {type: "item", id: 2},
-              {type: "container", id: 1, columns: [[], []]}
+              {type: "item", id: 2, itemUrl: "item"},
+              {type: "container", id: 1, itemUrl: "container", columns: [[], []]},
+              {type: "text", id: 3, itemUrl:"hui"}
           ],
           dropzones: {
-              "A": [
-                  {
-                      "type": "container",
-                      "id": 1,
-                      "columns": [
-                          [
-                              {
-                                  "type": "item",
-                                  "id": "1"
-                              },
-                              {
-                                  "type": "item",
-                                  "id": "2"
-                              }
-                          ],
-                          [
-                              {
-                                  "type": "item",
-                                  "id": "3"
-                              }
-                          ]
-                      ]
-                  },
-                  {
-                      "type": "item",
-                      "id": "4"
-                  },
-                  {
-                      "type": "item",
-                      "id": "5"
-                  },
-                  {
-                      "type": "item",
-                      "id": "6"
-                  }
-              ],
+
               "B": [
                   {
                       "type": "item",
@@ -127,6 +93,11 @@ component('createView', {
               ]
           }
       };
+      $scope.dropCallback = function(event, index, item, external, type, allowedType){
+          var result = document.getElementsByClassName("item ng-binding ng-scope");
+
+          return item;
+      }
 
       $scope.$watch('models.dropzones', function(model) {
           $scope.modelAsJson = angular.toJson(model, true);
